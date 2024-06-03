30936c1b67fd9c9a91ea6e73d17d35db6c277afe
geootrada <geootrada@yandex.ru>
2024-06-03 15:48:59 +0300

    1

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -11,36 +11,26 @@ namespace ConsoleApp18
         static void Main(string[] args)
         {
             string userName;
-            string userSymbol;
-            int numberSymbolName;
-            int additionalSymbol = 2;
-            int numberSymbolString;
-            string someString;
+            char userSymbol;
+            string middleString;
 
             Console.WriteLine("Здравствуйте, сейчас мы вместе создадим красивую картинку.");
-            Console.Write("Введите ваше имя: ");
+            Console.WriteLine("Введите ваше имя: ");
             userName = Console.ReadLine();
-            Console.Write("Введите любой символ: ");
-            userSymbol = Console.ReadLine();
-            numberSymbolName = userName.Length;
-            numberSymbolString = numberSymbolName + additionalSymbol;
+            Console.WriteLine("Введите любой символ: ");
+            userSymbol = Console.ReadKey(true).KeyChar;
+            middleString = userSymbol + userName + userSymbol;
 
-            while (numberSymbolString > 0)
-            {
-                Console.Write(userSymbol);
-                numberSymbolString --;
-            }
+            for (int i = 0; i < middleString.Length; i++)
 
+            Console.Write(userSymbol);
             Console.WriteLine();
-            Console.WriteLine(userSymbol + userName + userSymbol);
-            numberSymbolString = numberSymbolName + additionalSymbol;
 
-            while (numberSymbolString > 0)
-            {
-                Console.Write(userSymbol);
-                (numberSymbolString) --;
-            }
+            Console.WriteLine(middleString);
 
+            for (int i = 0; i < middleString.Length; i++)
+
+            Console.Write(userSymbol);
             Console.WriteLine();
         }
     }
